eb2e2587fbb9c2e5c983ff1e3ca3d786efdb245e
Aisha Al Attiyah <aattiyah@andrew.cmu.edu>
2024-10-14 22:33:58 +0300

    added route for GET & POST in admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,57 +5,57 @@ const helpers = require('./helpers');
 module.exports = function (app, name, middleware, controllers) {
     const middlewares = [middleware.pluginHooks];
 
-    helpers.setupAdminPageRoute(app, `${name}`, middlewares, controllers.admin.routeIndex);
+    helpers.setupAdminPageRoute(app, `/${name}`, middlewares, controllers.admin.routeIndex);
 
-    helpers.setupAdminPageRoute(app, `${name}/dashboard`, middlewares, controllers.admin.dashboard.get);
-    helpers.setupAdminPageRoute(app, `${name}/dashboard/logins`, middlewares, controllers.admin.dashboard.getLogins);
-    helpers.setupAdminPageRoute(app, `${name}/dashboard/users`, middlewares, controllers.admin.dashboard.getUsers);
-    helpers.setupAdminPageRoute(app, `${name}/dashboard/topics`, middlewares, controllers.admin.dashboard.getTopics);
-    helpers.setupAdminPageRoute(app, `${name}/dashboard/searches`, middlewares, controllers.admin.dashboard.getSearches);
-    helpers.setupAdminPageRoute(app, `${name}/dashboard/bug-logs`, middlewares, controllers.admin.dashboard.getBugLogs);
+    helpers.setupAdminPageRoute(app, `/${name}/dashboard`, middlewares, controllers.admin.dashboard.get);
+    helpers.setupAdminPageRoute(app, `/${name}/dashboard/logins`, middlewares, controllers.admin.dashboard.getLogins);
+    helpers.setupAdminPageRoute(app, `/${name}/dashboard/users`, middlewares, controllers.admin.dashboard.getUsers);
+    helpers.setupAdminPageRoute(app, `/${name}/dashboard/topics`, middlewares, controllers.admin.dashboard.getTopics);
+    helpers.setupAdminPageRoute(app, `/${name}/dashboard/searches`, middlewares, controllers.admin.dashboard.getSearches);
+    helpers.setupAdminPageRoute(app, `/${name}/dashboard/bug-logs`, middlewares, controllers.admin.dashboard.getBugLogs);
 
-    helpers.setupAdminPageRoute(app, `${name}/manage/categories`, middlewares, controllers.admin.categories.getAll);
-    helpers.setupAdminPageRoute(app, `${name}/manage/categories/:category_id`, middlewares, controllers.admin.categories.get);
-    helpers.setupAdminPageRoute(app, `${name}/manage/categories/:category_id/analytics`, middlewares, controllers.admin.categories.getAnalytics);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/categories`, middlewares, controllers.admin.categories.getAll);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/categories/:category_id`, middlewares, controllers.admin.categories.get);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/categories/:category_id/analytics`, middlewares, controllers.admin.categories.getAnalytics);
 
-    helpers.setupAdminPageRoute(app, `${name}/manage/privileges/:cid?`, middlewares, controllers.admin.privileges.get);
-    helpers.setupAdminPageRoute(app, `${name}/manage/tags`, middlewares, controllers.admin.tags.get);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/privileges/:cid?`, middlewares, controllers.admin.privileges.get);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/tags`, middlewares, controllers.admin.tags.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/manage/users`, middlewares, controllers.admin.users.index);
-    helpers.setupAdminPageRoute(app, `${name}/manage/registration`, middlewares, controllers.admin.users.registrationQueue);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/users`, middlewares, controllers.admin.users.index);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/registration`, middlewares, controllers.admin.users.registrationQueue);
 
-    helpers.setupAdminPageRoute(app, `${name}/manage/admins-mods`, middlewares, controllers.admin.adminsMods.get);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/admins-mods`, middlewares, controllers.admin.adminsMods.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/manage/groups`, middlewares, controllers.admin.groups.list);
-    helpers.setupAdminPageRoute(app, `${name}/manage/groups/:name`, middlewares, controllers.admin.groups.get);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/groups`, middlewares, controllers.admin.groups.list);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/groups/:name`, middlewares, controllers.admin.groups.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/manage/uploads`, middlewares, controllers.admin.uploads.get);
-    helpers.setupAdminPageRoute(app, `${name}/manage/digest`, middlewares, controllers.admin.digest.get);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/uploads`, middlewares, controllers.admin.uploads.get);
+    helpers.setupAdminPageRoute(app, `/${name}/manage/digest`, middlewares, controllers.admin.digest.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/settings/email`, middlewares, controllers.admin.settings.email);
-    helpers.setupAdminPageRoute(app, `${name}/settings/user`, middlewares, controllers.admin.settings.user);
-    helpers.setupAdminPageRoute(app, `${name}/settings/post`, middlewares, controllers.admin.settings.post);
-    helpers.setupAdminPageRoute(app, `${name}/settings/advanced`, middlewares, controllers.admin.settings.advanced);
-    helpers.setupAdminPageRoute(app, `${name}/settings/navigation`, middlewares, controllers.admin.settings.navigation);
-    helpers.setupAdminPageRoute(app, `${name}/settings/api`, middlewares, controllers.admin.settings.api);
-    helpers.setupAdminPageRoute(app, `${name}/settings/:term?`, middlewares, controllers.admin.settings.get);
+    helpers.setupAdminPageRoute(app, `/${name}/settings/email`, middlewares, controllers.admin.settings.email);
+    helpers.setupAdminPageRoute(app, `/${name}/settings/user`, middlewares, controllers.admin.settings.user);
+    helpers.setupAdminPageRoute(app, `/${name}/settings/post`, middlewares, controllers.admin.settings.post);
+    helpers.setupAdminPageRoute(app, `/${name}/settings/advanced`, middlewares, controllers.admin.settings.advanced);
+    helpers.setupAdminPageRoute(app, `/${name}/settings/navigation`, middlewares, controllers.admin.settings.navigation);
+    helpers.setupAdminPageRoute(app, `/${name}/settings/api`, middlewares, controllers.admin.settings.api);
+    helpers.setupAdminPageRoute(app, `/${name}/settings/:term?`, middlewares, controllers.admin.settings.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/appearance/:term?`, middlewares, controllers.admin.appearance.get);
+    helpers.setupAdminPageRoute(app, `/${name}/appearance/:term?`, middlewares, controllers.admin.appearance.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/extend/plugins`, middlewares, controllers.admin.plugins.get);
-    helpers.setupAdminPageRoute(app, `${name}/extend/widgets`, middlewares, controllers.admin.extend.widgets.get);
-    helpers.setupAdminPageRoute(app, `${name}/extend/rewards`, middlewares, controllers.admin.extend.rewards.get);
+    helpers.setupAdminPageRoute(app, `/${name}/extend/plugins`, middlewares, controllers.admin.plugins.get);
+    helpers.setupAdminPageRoute(app, `/${name}/extend/widgets`, middlewares, controllers.admin.extend.widgets.get);
+    helpers.setupAdminPageRoute(app, `/${name}/extend/rewards`, middlewares, controllers.admin.extend.rewards.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/advanced/database`, middlewares, controllers.admin.database.get);
-    helpers.setupAdminPageRoute(app, `${name}/advanced/events`, middlewares, controllers.admin.events.get);
-    helpers.setupAdminPageRoute(app, `${name}/advanced/hooks`, middlewares, controllers.admin.hooks.get);
-    helpers.setupAdminPageRoute(app, `${name}/advanced/logs`, middlewares, controllers.admin.logs.get);
-    helpers.setupAdminPageRoute(app, `${name}/advanced/errors`, middlewares, controllers.admin.errors.get);
-    helpers.setupAdminPageRoute(app, `${name}/advanced/errors/export`, middlewares, controllers.admin.errors.export);
-    helpers.setupAdminPageRoute(app, `${name}/advanced/cache`, middlewares, controllers.admin.cache.get);
+    helpers.setupAdminPageRoute(app, `/${name}/advanced/database`, middlewares, controllers.admin.database.get);
+    helpers.setupAdminPageRoute(app, `/${name}/advanced/events`, middlewares, controllers.admin.events.get);
+    helpers.setupAdminPageRoute(app, `/${name}/advanced/hooks`, middlewares, controllers.admin.hooks.get);
+    helpers.setupAdminPageRoute(app, `/${name}/advanced/logs`, middlewares, controllers.admin.logs.get);
+    helpers.setupAdminPageRoute(app, `/${name}/advanced/errors`, middlewares, controllers.admin.errors.get);
+    helpers.setupAdminPageRoute(app, `/${name}/advanced/errors/export`, middlewares, controllers.admin.errors.export);
+    helpers.setupAdminPageRoute(app, `/${name}/advanced/cache`, middlewares, controllers.admin.cache.get);
 
-    helpers.setupAdminPageRoute(app, `${name}/development/logger`, middlewares, controllers.admin.logger.get);
-    helpers.setupAdminPageRoute(app, `${name}/development/info`, middlewares, controllers.admin.info.get);
+    helpers.setupAdminPageRoute(app, `/${name}/development/logger`, middlewares, controllers.admin.logger.get);
+    helpers.setupAdminPageRoute(app, `/${name}/development/info`, middlewares, controllers.admin.info.get);
 
     apiRoutes(app, name, middleware, controllers);
 };
@@ -68,6 +68,7 @@ function apiRoutes(router, name, middleware, controllers) {
     router.get(`/api/${name}/advanced/cache/dump`, middleware.ensureLoggedIn, helpers.tryRoute(controllers.admin.cache.dump));
 
     router.get(`/api/${name}/get-bug-log`, middleware.ensureLoggedIn, helpers.tryRoute(controllers.admin.dashboard.getBugLogs));
+    router.post(`/api/${name}/submit-bug-report`, middleware.ensureLoggedIn, helpers.tryRoute(controllers.admin.dashboard.submitBugReport));
     
     const multipart = require('connect-multiparty');
     const multipartMiddleware = multipart();
