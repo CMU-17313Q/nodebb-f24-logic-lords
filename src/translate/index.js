b417b40ececde5005e6653a7f015e7000f7f95f0
Mohamed Elzeni <msmohame@andrew.cmu.edu>
2024-11-17 13:50:46 +0300

    Update translator service URL

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,7 +3,8 @@
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-	const TRANSLATOR_API = 'https://translation-service-logic-lords.azurewebsites.net';
+	// const TRANSLATOR_API = 'https://nodebb-f24-translator.azurewebsites.net/';
+	const TRANSLATOR_API = 'https://translation-service-logic-lords.azurewebsites.net/';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
 	return [data.is_english, data.translated_content];
